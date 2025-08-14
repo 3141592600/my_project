0a175af629e7f065ed8176cd7e314a249e332db2
3141592600 <117092956+3141592600@users.noreply.github.com>
2025-08-14 18:11:31 +0800

    Delete .devcontainer directory

--- .devcontainer/scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 等待 MySQL 启动
-echo "等待 MySQL 启动..."
-sleep 10
-
-echo "初始化数据库..."
-docker exec -i mysql mysql -uroot -p123456 master_db < /workspaces/my_project/mysql-init.sql
-
-echo "Spring Boot 和 Vue 容器由 docker-compose command 自动启动"
