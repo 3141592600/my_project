0d55e827126c3ea4cddefbc8c04ab7cbc258621b
3141592600 <117092956+3141592600@users.noreply.github.com>
2025-08-14 13:47:57 +0800

    Update init.sh

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -5,13 +5,13 @@ echo "等待 MySQL 启动..."
 sleep 10
 
 echo "初始化数据库..."
-docker exec -i mysql mysql -uroot -p123456 master_db < /workspace/mysql-init.sql
+docker exec -i mysql mysql -uroot -p123456 master_db < /workspaces/my_project/mysql-init.sql
 
 echo "启动 Spring Boot..."
-cd /workspace/backend
+cd /workspaces/my_project/backend
 mvn spring-boot:run &
 
 echo "启动 Vue..."
-cd /workspace/frontend
+cd /workspaces/my_project/frontend
 npm install
 npm run dev -- --host 0.0.0.0 &
